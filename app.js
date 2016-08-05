8e9b41c9d4b51e052f7fb2ddaab5b5880bcfc97d
David Haley <djdavidhaley@gmail.com>
2016-08-04 20:55:42 -0600

    Configured Jade, Bower and JQuery

--- app.js
+++ app.js
@@ -2,12 +2,10 @@
 
 var express = require("express");
 var app = express();
-const Twitter = require("twitter-node-client").Twitter;
-
-var rp = require("request-promise");
 
 const URL = "https://api.twitter.com/1.1/search/tweets.json&q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4";
 
+const Twitter = require("twitter-node-client").Twitter;
 
 const twitterConfig = {
   consumerKey: "0yjFJ85xFKKZywszmJh5ISBuA",
@@ -17,7 +15,7 @@ const twitterConfig = {
   callBackUrl: ""
 };
 
-var twitter = new Twitter(twitterConfig);
+let twitter = new Twitter(twitterConfig);
 
 app.get("/", function (req, res) {
   twitter.getSearch({"q":" movie -scary :)", "count": 10, "result\_type":"popular"}, function() {}, function(response) {
@@ -25,6 +23,9 @@ app.get("/", function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+module.exports = app;
+
+// app.listen(3000, function () {
+//   console.log('Example app listening on port 3000!');
+// });
+
