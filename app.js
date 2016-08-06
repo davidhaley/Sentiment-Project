6c7b42ad63fba2c90b728a1971f4f494ff562c14
David Haley <djdavidhaley@gmail.com>
2016-08-05 19:06:14 -0600

    Add write to text file for Twitter GET request

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var express = require("express");
 var app = express();
 var unirest = require('unirest');
+var fs = require('fs');
 
 const URL = "https://api.twitter.com/1.1/search/tweets.json&q=%23freebandnames&since_id=24012619984051000&max_id=250126199840518145&result_type=mixed&count=4";
 
@@ -21,15 +22,24 @@ var twitter = new Twitter(twitterConfig);
 app.get("/", function (req, res) {
   twitter.getSearch({"q":" movie -scary :)", "count": 10, "result\_type":"popular"}, function() {}, function(response) {
     res.render("index", {title: "Express", response: response});
+
+    fs.writeFile('twitter.txt', response, function (err) {
+      if (err) return console.log(err);
+      console.log(response);
+    });
   });
 });
 
-unirest.get("https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=great+value+in+its+price+range!")
-.header("X-Mashape-Key", "kWBJRsZrjmmshQnhz4Fta1chiRRxp1rhKxgjsnUGdwGKSkVFbG")
-.header("Accept", "application/json")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
+
+
+
+
+// unirest.get("https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=great+value+in+its+price+range!")
+// .header("X-Mashape-Key", "kWBJRsZrjmmshQnhz4Fta1chiRRxp1rhKxgjsnUGdwGKSkVFbG")
+// .header("Accept", "application/json")
+// .end(function (result) {
+//   console.log(result.status, result.headers, result.body);
+// });
 
 module.exports = app;
 // module.exports = unirest;
