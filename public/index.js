7ef8e612bd26653607399bc904be6391d2fcd30b
David Haley <davidhaley@protonmail.com>
2016-08-10 15:23:54 -0600

    Add sentiment to index.ejs page

--- public/index.js
+++ public/index.js
@@ -1,7 +1,8 @@
+"use strict";
+
 $(document).ready(function(){
   $("#load-tweets").on('click', function(callback) {
     console.log("button has been clicked!");
-
     $.ajax({
       type: "POST",
       url: '  /tweets',
@@ -16,11 +17,30 @@ $(document).ready(function(){
           $(div).append(p);
           $('.tweets').append(div);
         });
+        $.ajax({
+          type: "GET",
+          url: '  /sentiment',
+          dataType: 'JSON',
+          success: function(data) {
+            data.forEach(function(sentiment) {
+              var div = $('<div>');
+              var p = $('<p>');
+
+              $(p).append(sentiment);
+              $(div).append(p);
+              $('.sentiment').append(div);
+            });
+          },
+          error: function(data) {
+            console.log("error");
+            console.log(data);
+          }
+      });
       },
-      error: function(data) {
-        console.log("error");
-        console.log(data);
-      }
+        error: function(data) {
+          console.log("error");
+          console.log(data);
+        }
+      });
     });
   });
-});
