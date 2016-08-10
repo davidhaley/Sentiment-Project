387bf9640bc6b60a9a0d127d9e41eb17b5c23b5f
David Haley <djdavidhaley@gmail.com>
2016-08-10 15:23:54 -0600

    Add sentiment to index.ejs page

--- app.js
+++ app.js
@@ -30,11 +30,7 @@ app.get('/', function(req, res) {
 
 app.post('/tweets', function(req, res) {
 
-  var sentimentArray = [];
-  var apiDomain = "https://twinword-sentiment-analysis.p.mashape.com/analyze/?text="
-
-  async.waterfall([
-    function(callback) {
+    function getTweets(callback) {
       var error = function (err, response, body) {
         if (err) {
           console.log('ERROR [%s]', err);
@@ -45,6 +41,7 @@ app.post('/tweets', function(req, res) {
       var success = function(data) {
         var contentArray = [];
         var sentimentQueries = [];
+
         JSON.parse(data).statuses.forEach(function(tweet) {
           var text = tweet.text;
           var query = text.split(" ").join("+").replace("'","");
@@ -52,105 +49,45 @@ app.post('/tweets', function(req, res) {
           contentArray.push(tweet);
         });
 
+        res.app.locals.sentimentQueries = sentimentQueries;
         res.json(contentArray);
-        callback(null, sentimentQueries);
+        // callback(null, sentimentQueries);
       };
-      twitter.getSearch({"q":"trump", "lang":"en", "count": 20, "result\_type":"popular"}, error, success);
-    },
-    function getSentiment(sentimentQueries, callback) {
-      async.map(sentimentQueries, function(query, callback) {
-        unirest.get(apiDomain+query)
-        .header("X-Mashape-Key", "kWBJRsZrjmmshQnhz4Fta1chiRRxp1rhKxgjsnUGdwGKSkVFbG")
-        .header("Accept", "application/json")
-        .end(function (result) {
-          if (result.status == 200) {
-            console.log("Result status 200. Success");
-            var res = [result.body.type, result.status];
-            callback(null, res);
-            return;
-          } else {
-            console.log("Result status is " + "result.status");
-            var res = [result.body.type, result.status];
-            callback(null, res);
-            return;
-          }
-        });
-        sentimentArray.push(res);
-      }, function(err, results) {
-        // callback(null, results);
-        sentimentArray = results;
-        var test = [ [ 'negative', 200 ],
-          [ 'negative', 200 ],
-          [ 'negative', 200 ],
-          [ 'positive', 200 ],
-          [ 'neutral', 200 ],
-          [ 'negative', 200 ],
-          [ 'neutral', 200 ],
-          [ 'negative', 200 ],
-          [ 'neutral', 200 ],
-          [ 'neutral', 200 ],
-          [ 'positive', 200 ],
-          [ 'neutral', 200 ],
-          [ 'neutral', 200 ],
-          [ 'positive', 200 ],
-          [ 'negative', 200 ] ]
-        console.log(JSON.stringify(test) == JSON.stringify(results));
-      });
-  //   // callback(null, sentimentArray);
-  //   // debugger;
-  //   }
-  // // }
-  // ],
-  // // optional callback
-  // function(err, results) {
-  //   app.locals.sentimentArray = results;
-  //   debugger;
-  // });
+      twitter.getSearch({"q":"trump", "lang":"en", "count": 3, "result\_type":"popular"}, error, success);
     }
-  ]);
+    getTweets();
 });
 
-// When a browser requests the root apiDomain, request tweet, send to NPL, and display on index page.
-// app.get('/', function(req, res){
+app.get('/sentiment', function(req, res) {
 
-//   async.waterfall([
+  var apiDomain = "https://twinword-sentiment-analysis.p.mashape.com/analyze/?text="
+  var sentimentArray = [];
 
-//     // Send array of tweets to NLP API to be evaluated, return 'positive/negative' sentiment on success, or print error on failure.
-//     function getSentiment(contentArray, callback) {
-//       contentArray.forEach(function(tweet, index) {
-//         var text = tweet.text;
-//         var query = text.split(" ").join("+").replace("'","");
-//         var apiDomain = "https://twinword-sentiment-analysis.p.mashape.com/analyze/?text="
-
-//       unirest.get(apiDomain+query)
-//         .header("X-Mashape-Key", "kWBJRsZrjmmshQnhz4Fta1chiRRxp1rhKxgjsnUGdwGKSkVFbG")
-//         .header("Accept", "application/json")
-//         .end(function (result) {
-//           if (result.status == 200) {
-//             console.log("Result status 200. Success");
-//             contentArray[index].sentiment = result.body.type;
-//             contentArray[index].status = result.status
-//             return;
-//           } else {
-//             console.log("Result status is " + "result.status");
-//             res.locals.sentiment = "ERROR"
-//             res.locals.status = result.status;
-//             return;
-//           }
-//         });
-//       });
-//       console.log(contentArray);
-//       callback(null, contentArray);
-//     }
-//   ], function(err, result) {
-//     if (err) {
-//       console.log("ERROR: " + err);
-//     } else {
-//       console.log("SUCCESS: " + result);
-//       res.render('index.ejs', {contentArray: result});
-//     }
-//   });
-// });
+  function getSentiment(sentimentQueries, callback) {
+    async.map(sentimentQueries, function(query, callback) {
+      unirest.get(apiDomain+query)
+      .header("X-Mashape-Key", "kWBJRsZrjmmshQnhz4Fta1chiRRxp1rhKxgjsnUGdwGKSkVFbG")
+      .header("Accept", "application/json")
+      .end(function (result) {
+        if (result.status == 200) {
+          console.log("Result status 200. Success");
+          var res = [result.body.type, result.status];
+          callback(null, res);
+          return;
+        } else {
+          console.log("Result status is " + "result.status");
+          var res = [result.body.type, result.status];
+          callback(null, res);
+          return;
+        }
+      });
+      sentimentArray.push(res);
+    }, function(err, results) {
+      res.json(results);
+    });
+  };
+  getSentiment(res.app.locals.sentimentQueries);
+});
 
 app.listen(3000);
 console.log('app is listening at localhost:3000');
