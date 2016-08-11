60c26d86c75d51f70069d8c5596ae687306eebd1
David Haley <djdavidhaley@gmail.com>
2016-08-11 14:21:04 -0600

    Handle null values from sentiment results

--- public/index.js
+++ public/index.js
@@ -1,46 +1,57 @@
 "use strict";
 
 $(document).ready(function(){
-  $("#load-tweets").on('click', function(callback) {
-    console.log("button has been clicked!");
-    $.ajax({
-      type: "POST",
-      url: '  /tweets',
-      dataType: 'JSON',
-      success: function(data) {
-        data.forEach(function(tweet) {
-          var div = $('<div>');
-          var p = $('<p>');
-          var text = tweet.text;
+  $("#load-tweets").keyup(function(event) {
+    event.preventDefault();
+    if (event.keyCode == '13') {
+      console.log("button has been clicked!");
+      $.ajax({
+        type: "POST",
+        url: '  /tweets',
+        dataType: 'JSON',
+        success: function(data) {
+          data.forEach(function(tweet) {
+            var div = $('<div>');
+            var p = $('<p>');
+            var text = tweet.text;
+            var id = tweet.id;
 
-          $(p).append(text);
-          $(div).append(p);
-          $('.tweets').append(div);
+            $(p).append(text).append(id);
+            $(div).attr('id', id).append(p);
+            $('.tweets').append(div);
+          });
+          $.ajax({
+            type: "GET",
+            url: '  /sentiment',
+            dataType: 'JSON',
+            success: function(data) {
+              data.forEach(function(sentiment) {
+                var div = $('<div>');
+                var p = $('<p>');
+
+                if (sentiment == null) {
+                  false;
+                  return;
+                } else {
+                  var id = sentiment[0];
+                  var text = $(p).append(sentiment[1]);
+                  $(div).append(text).append(id);
+                  $('.tweets').children("#" + id).append(div);               
+                }
+              });
+            },
+            error: function(data) {
+              console.log("error");
+              console.log(data);
+            }
         });
-        $.ajax({
-          type: "GET",
-          url: '  /sentiment',
-          dataType: 'JSON',
-          success: function(data) {
-            data.forEach(function(sentiment) {
-              var div = $('<div>');
-              var p = $('<p>');
-
-              $(p).append(sentiment[0]);
-              $(div).append(p);
-              $('.sentiment').append(div);
-            });
-          },
+        },
           error: function(data) {
             console.log("error");
             console.log(data);
           }
       });
-      },
-        error: function(data) {
-          console.log("error");
-          console.log(data);
-        }
-      });
-    });
+    }
+    return false;
   });
+});
